02235243d504c0686a122cf5bab93e33fbcdfd98
Jakob Siebenhütter <jakob.siebenhuetter@gmx.at>
2026-05-30 13:58:49 +0200

    add: tailwindcss

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,18 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 
+import tailwindcss from '@tailwindcss/vite';
+
 // https://astro.build/config
 export default defineConfig({
-    devToolbar: {
-        enabled: false
-    },
-    site: 'https://jakobsiebenhuetter.github.io',
-    base: '/Blog-Web'
+  devToolbar: {
+      enabled: false
+  },
+
+  site: 'https://jakobsiebenhuetter.github.io',
+  base: '/Blog-Web',
+
+  vite: {
+    plugins: [tailwindcss()]
+  }
 });
